e5abc29ae6a72e62c7945ad7ff82c0700950b7bc
Peshal <ppeshalmani@gmail.com>
2025-04-25 14:03:57 +0545

    Fix type problems

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -7,9 +11,9 @@ const PORT = process.env.PORT || 8000;
 const isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 
